3fe8a4f3914c0be7b4a25aa7febfd12649d60c77
C.Viktor <vchekryzhov@ya.ru>
2020-03-22 02:08:57 +0300

    typo fix

--- app/controllers/api/v2/device_controller.rb
+++ app/controllers/api/v2/device_controller.rb
@@ -1,6 +1,6 @@
 class Api::V2::DeviceController < ApiController
   def show
-    render json: DeviceSerializer.new(Device.find(params[:id]))
+    render json: DeviceSerializer.new(Device.find(params[:id])).as_json
   end
 
 end
