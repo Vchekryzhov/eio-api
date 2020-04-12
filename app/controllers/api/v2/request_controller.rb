8826cec260b06d30133eef370060c47950248f9c
C.Viktor <vchekryzhov@ya.ru>
2020-04-13 01:09:15 +0300

    request

--- app/controllers/api/v2/request_controller.rb
+++ app/controllers/api/v2/request_controller.rb
@@ -2,7 +2,6 @@ class Api::V2::RequestController < ApiController
 before_action :accept_all_params
 
   def create
-    byebug
     Request.create(request: params[:request])
     render json: {}, status: 200
   end
