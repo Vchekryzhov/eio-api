a6eb54846edd5b1a65c926bafb9a0f077e211d2c
C.Viktor <vchekryzhov@ya.ru>
2020-04-12 23:59:11 +0300

    log

--- app/controllers/api/v2/request_controller.rb
+++ app/controllers/api/v2/request_controller.rb
@@ -2,6 +2,7 @@ class Api::V2::RequestController < ApiController
 before_action :accept_all_params
 
   def create
+    logger.info(params[:data])
     Request.create(request: params[:datum])
     render json: {}, status: 200
   end
