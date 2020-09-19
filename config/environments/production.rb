918310780a786318e2d389f6c6d4f6a193c6d2c9
Виктор <vchekryzhov@ya.ru>
2020-09-19 20:57:57 +0300

    api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-    config.action_controller.default_url_options = { host: 'cnc.kovalev.team', port: 80 }
+    config.action_controller.default_url_options = { host: 'cnc-api.kovalev.team', port: 80 }
 
 
   # Inserts middleware to perform automatic connection switching.
