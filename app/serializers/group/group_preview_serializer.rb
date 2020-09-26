b2fb0e608d9ada01533a8113dfdb194a19d28ff2
Виктор <vchekryzhov@ya.ru>
2020-09-26 19:52:56 +0300

    device id

--- app/serializers/group/group_preview_serializer.rb
+++ app/serializers/group/group_preview_serializer.rb
@@ -1,8 +1,3 @@
 class Group::GroupPreviewSerializer < GroupBaseSerializer
-  attributes :id, :parent_id, :group_type_id, :devices
-  def devices
-    object.devices.map do |device|
-      DeviceSerializer.new(device)
-    end
-  end
+  attributes :id, :parent_id, :group_type_id, :device_id
 end
