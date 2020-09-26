b57efc62364a63fb63b7bc4662832bf5b1ed6643
Виктор <vchekryzhov@ya.ru>
2020-09-26 18:18:09 +0300

    valid

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,6 +1,8 @@
 class Input < ApplicationRecord
   belongs_to :input_type
   belongs_to :group
+  validates :input_type, :group, presence: true
+  validates :input_type, :group, presence: { message: "can't create imputs with attributes #{attributes}" }
   def input_type=(v)
     super(InputType.find_by_kind(v))
   end
