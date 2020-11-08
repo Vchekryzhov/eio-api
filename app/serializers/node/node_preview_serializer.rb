9c2d433872296b765d14875db9918aa056502023
C.Viktor <vchekryzhov@ya.ru>
2020-11-08 18:19:15 +0300

    color

--- app/serializers/node/node_preview_serializer.rb
+++ app/serializers/node/node_preview_serializer.rb
@@ -1,5 +1,5 @@
 class Node::NodePreviewSerializer < NodeBaseSerializer
-  attributes :id, :title, :image, :preview_description
+  attributes :id, :title, :image, :preview_description, :image_color
   def image
     object.image.img_proxy_url({width: 150, height: 150})
   end
