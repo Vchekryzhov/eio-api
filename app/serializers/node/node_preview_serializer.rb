f97b0c3ddedf2afb22d7fb4adf32ff15f368c8c7
C.Viktor <vchekryzhov@ya.ru>
2020-11-08 18:16:37 +0300

    image

--- app/serializers/node/node_preview_serializer.rb
+++ app/serializers/node/node_preview_serializer.rb
@@ -1,6 +1,6 @@
 class Node::NodePreviewSerializer < NodeBaseSerializer
   attributes :id, :title, :image, :preview_description
   def image
-    object.image.img_proxy_url
+    object.image.img_proxy_url({width: 150, height: 150})
   end
 end
